58449ba7c77bd76ebfd96c6192e88918caf61f4b
Y-I-sp <nifty.plala@gmail.com>
2023-07-22 13:48:22 +0900

    MUIの追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,11 @@ import 'semantic-ui-css/semantic.min.css'
 import {Loader} from 'semantic-ui-react'
 import{ IndexPageProps } from '../components/interface'
 import { fetchCatImage,getServerSideProps } from '../components/initialCat';
-import {WebName} from '../components/Head'
-     <WebName />
+import { Header } from '../components/header';
+import styles from '../styles/Home.module.css'
+import Button from '@mui/material/Button';
 const Home: NextPage<IndexPageProps> = ({initialCatImageUrl}) => {
+
   const [catImageUrl,setCatImageUrl] = useState(initialCatImageUrl);
   const [isLoading,setIsLoading] = useState(false)
 
@@ -18,24 +20,18 @@ const Home: NextPage<IndexPageProps> = ({initialCatImageUrl}) => {
   }
 
   return (
-    <div style={{
-      display:"flex",
-      flexDirection:"column",
-      alignItems:"center",
-      justifyContent:"center",
-      height:"100vh"
-
-    }}>
+    <div className={styles.container}>
+       <Header/>
       <h1>猫画像アプリ</h1>
       {isLoading ? (
         <Loader active size="huge" inline="centered"/>
         ):(<img src={catImageUrl}height="auto"/>
         )}
       
+      <Button variant="contained" color="success" onClick={handleClick}>
+  今日の猫さん
+</Button>
       
-      <button style={{ marginTop:18}} onClick={handleClick}>
-        今日の猫さん
-        </button>
     </div>
   )
   
