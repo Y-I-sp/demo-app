d54942af3ae57b2fdc42297142deb05e245e4863
Y-I-sp <nifty.plala@gmail.com>
2023-08-02 19:33:03 +0900

    Index.tsxをコンポーネントの置き場に修正

---
+++ components/Home.tsx
@@ -0,0 +1,45 @@
+import {Loader} from 'semantic-ui-react'
+import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next'
+import { useState } from 'react';
+import{ IndexPageProps,SerchCatImage } from './interface'
+import type { GetServerSideProps } from 'next'
+import Button from '@mui/material/Button';
+
+export  const fetchCatImage = async ():Promise<SerchCatImage>=>{
+  const res = await fetch("https://api.thecatapi.com/v1/images/search");
+  const result = await res.json();
+  return result[0];
+}
+
+   const Home: NextPage<any> = ({initialCatImageUrl}) => {
+        const [catImageUrl,setCatImageUrl] = useState(initialCatImageUrl);
+        const [isLoading,setIsLoading] = useState(false)
+      
+        const handleClick = async () => {
+          setIsLoading(true);
+          const catImage = await fetchCatImage();
+          setCatImageUrl(catImage.url);
+          setIsLoading(false)
+        }
+   return(
+     <div>
+         <div className={styles.main}>
+                {isLoading ? (<Loader active size="huge" inline="centered"/>):
+                             (<img src={catImageUrl}width="1000" height="800"/>)
+                }
+         </div> 
+         <div className={styles.footer}>
+          <Button variant="contained" color="success" onClick={handleClick}>今日の猫さん</Button>
+         </div>
+     </div>
+         )
+}
+   
+export const getServerSideProps: GetServerSideProps<IndexPageProps> = async () =>{
+        const catImage = await fetchCatImage();
+return{
+      props:{initialCatImageUrl:catImage.url,}
+      }
+}
+export default Home
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,52 +1,17 @@
-import type { NextPage } from 'next'
-import { useState } from 'react';
 import 'semantic-ui-css/semantic.min.css'
-import {Loader} from 'semantic-ui-react'
-import{ IndexPageProps,SerchCatImage } from '../components/interface'
-import type { GetServerSideProps } from 'next'
 import { Header } from '../components/header';
-import styles from '../styles/Home.module.css'
-import Button from '@mui/material/Button';
+import Home  from '../components/Home'
 
-export const fetchCatImage = async ():Promise<SerchCatImage>=>{
-  const res = await fetch("https://api.thecatapi.com/v1/images/search");
-  const result = await res.json();
-  // console.log(result[0]);
-  return result[0];
+function Index(){
+return(
+<div>
+     <Header/>
+     <Home/>
+</div>
+)
 }
-<header><Header/></header>
+export default Index
 
-const Home: NextPage<IndexPageProps> = ({initialCatImageUrl}) => {
 
-  const [catImageUrl,setCatImageUrl] = useState(initialCatImageUrl);
-  const [isLoading,setIsLoading] = useState(false)
 
-  const handleClick = async () => {
-    setIsLoading(true);
-    const catImage = await fetchCatImage();
-    setCatImageUrl(catImage.url);
-    setIsLoading(false)
-  }
 
-  return (
-    <div className={styles.main}>
-      {isLoading ? (
-        <Loader active size="huge" inline="centered"/>
-        ):(<img src={catImageUrl}width="1000" height="800"/>
-        )}
-      
-      <Button variant="contained" color="success" onClick={handleClick}>今日の猫さん</Button>
-    </div>
-  )
-}
-
-export const getServerSideProps: GetServerSideProps<IndexPageProps> = async () =>{
-  const catImage = await fetchCatImage();
-  return{
-    props:{
-      initialCatImageUrl:catImage.url,
-    }
-  }
-}
-
-export default Home
