fc42e6c2bb17ca29a970e6e4f348517aaf98eb93
Y-I-sp <nifty.plala@gmail.com>
2023-08-03 18:07:10 +0900

    Headerの共通化

--- pages/Snowshoe.tsx
+++ pages/Snowshoe.tsx
@@ -1,58 +1,12 @@
-import type { NextPage } from 'next'
-import { useState } from 'react';
 import 'semantic-ui-css/semantic.min.css'
-import {Loader} from 'semantic-ui-react'
-import{ IndexPageProps,SerchCatImage } from '../components/interface'
-import type { GetServerSideProps } from 'next'
-import styles from '../styles/Home.module.css'
-import Button from '@mui/material/Button';
-import Link from 'next/link'
-
-export const fetchCatImage = async ():Promise<SerchCatImage>=>{
-  const res = await fetch("https://api.thecatapi.com/v1/images/search?breed_ids=snow");
-  const result = await res.json();
-  // console.log(result[0]);
-  return result[0];
+import { DetailHeader } from '../components/DetailHeader';
+import DetailHome  from '../components/DetailHome'
+function Index(){
+return(
+<div>
+     <DetailHeader breed='Snowshoe'/>
+     <DetailHome breed='Snowshoe'/>
+</div>
+)
 }
-const Home: NextPage<IndexPageProps> = ({initialCatImageUrl}) => {
-
-  const [catImageUrl,setCatImageUrl] = useState(initialCatImageUrl);
-  const [isLoading,setIsLoading] = useState(false)
-
-  const handleClick = async () => {
-    setIsLoading(true);
-    const catImage = await fetchCatImage();
-    setCatImageUrl(catImage.url);
-    setIsLoading(false)
-  }
-
-  return (
-    <div className={styles.main}>
-      
-      <div className={styles.container}>
-      <div className={styles.code}><Link href="/">Home</Link></div>
-      <div className={styles.code}><Link href='/Munchikin'>Munchikin</Link></div>
-      <div className={styles.code}><Link href='/Bengal'>Bengal</Link></div>
-      </div>
-        
-      {isLoading ? (
-        <Loader active size="huge" inline="centered"/>
-        ):(<img src={catImageUrl}height="auto"/>
-        )}
-      
-      <Button variant="contained" color="success" onClick={handleClick}>今日の猫さん</Button>
-      
-    </div>
-  )
-}
-
-export const getServerSideProps: GetServerSideProps<IndexPageProps> = async () =>{
-  const catImage = await fetchCatImage();
-  return{
-    props:{
-      initialCatImageUrl:catImage.url,
-    }
-  }
-}
-
-export default Home
+export default Index
